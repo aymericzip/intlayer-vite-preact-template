7bb6f8ef36153572bddf365ff201d84c88f5a51a
Aymeric PINEAU <ay.pineau@gmail.com>
2026-03-24 18:59:33 -0600

    chore: update packages

--- src/app.content.tsx
+++ src/app.content.tsx
@@ -1,38 +1,38 @@
-import { md, t, type Dictionary } from 'intlayer';
-import { type ComponentChildren } from 'preact';
+import { md, t, type Dictionary } from "intlayer";
+import { type ComponentChildren } from "preact";
 
 const appContent = {
-  key: 'app',
+  key: "app",
   content: {
     viteLogo: t({
-      en: 'Vite logo',
-      fr: 'Logo Vite',
-      es: 'Logo Vite',
+      en: "Vite logo",
+      fr: "Logo Vite",
+      es: "Logo Vite",
     }),
     preactLogo: t({
-      en: 'Preact logo',
-      fr: 'Logo Preact',
-      es: 'Logo Preact',
+      en: "Preact logo",
+      fr: "Logo Preact",
+      es: "Logo Preact",
     }),
 
-    title: 'Vite + Preact',
+    title: "Vite + Preact",
 
     count: t({
-      en: 'count is ',
-      fr: 'le compte est ',
-      es: 'el recuento es ',
+      en: "count is ",
+      fr: "le compte est ",
+      es: "el recuento es ",
     }),
 
     edit: t<ComponentChildren>({
-      en: md('Edit `src/app.tsx` and save to test HMR'),
-      fr: md('Éditez `src/app.tsx` et enregistrez pour tester HMR'),
-      es: md('Edita `src/app.tsx` y guarda para probar HMR'),
+      en: md("Edit `src/app.tsx` and save to test HMR"),
+      fr: md("Éditez `src/app.tsx` et enregistrez pour tester HMR"),
+      es: md("Edita `src/app.tsx` y guarda para probar HMR"),
     }),
 
     readTheDocs: t({
-      en: 'Click on the Vite and Preact logos to learn more',
-      fr: 'Cliquez sur les logos Vite et Preact pour en savoir plus',
-      es: 'Haga clic en los logotipos de Vite y Preact para obtener más información',
+      en: "Click on the Vite and Preact logos to learn more",
+      fr: "Cliquez sur les logos Vite et Preact pour en savoir plus",
+      es: "Haga clic en los logotipos de Vite y Preact para obtener más información",
     }),
   },
 } satisfies Dictionary;
